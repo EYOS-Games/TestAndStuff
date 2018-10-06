02fc8cad771ce6b8da9b5f81554833eb944fce30
Unknown <gennin@gmail.com>
2018-10-06 19:33:41 +0300

    first commit

---
+++ app/src/main/java/com/example/genni/testingandstuff/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.genni.testingandstuff;
+
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
